c51abd22395e78ebd35cdb456088e26c7293918e
Minwei Shen <minweishen1991@gmail.com>
2020-04-14 12:05:22 -0700

    add more loaders

--- webpack/common.js
+++ webpack/common.js
@@ -39,6 +39,30 @@ module.exports = {
     }, {
       test: /\.css$/i,
       use: ['style-loader', 'css-loader'],
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+        {
+          loader: '@svgr/webpack',
+          options: {
+            babel: false,
+            icon: true,
+          },
+        },
+      ],
+    }, {
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+        },
+      ],
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: ['file-loader'],
     }],
   },
 };
